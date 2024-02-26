0acdc64edc5d862206f885eb6eceeb2521c8d5ed
Brandon Antonio Marroquin Pérez <3045062060114@ingenieria.usac.edu.gt>
2024-02-25 23:17:29 -0600

    ver. beta 0.00.01

---
+++ IPC1_Proyecto1_202300813/src/ipc1_proyecto1_202300813/IPC1_Proyecto1_202300813.java
@@ -0,0 +1,12 @@
+
+package ipc1_proyecto1_202300813;
+
+
+public class IPC1_Proyecto1_202300813 {
+
+
+    public static void main(String[] args) {
+        System.out.println(" hola mundo");
+    }
+    
+}
